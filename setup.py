2c226ffd5c928cb1f7fe113dce76f14bc01d3a5e
Dan Walsh <dwalsh@redhat.com>
2015-01-14 17:10:00 -0500

    Initial version

---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# Author: Dan Walsh <dwalsh@redhat.com>
+import os
+from distutils.core import setup
+
+setup(name = "atom", scripts=["atom"], version="1.0", description="Atomic Container Management Tool", author="Daniel Walsh", author_email="dwalsh@redhat.com")
