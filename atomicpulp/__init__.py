b0ba962170c6aee60a6bc3f34ed3d5e1191c3ded
Dan Walsh <dwalsh@redhat.com>
2015-04-24 13:45:10 -0400

    Add ability to upload/push to a registry
    This patch will allow users to upload to either a docker registry or
    to a pulp crane server.  We want to support Sattelite in the future as
    well.
    
    Will read authorization data out of ~/.pulp/admin.conf if it exists, otherwise
    it will prompt for Username and Password.
    
    Most of pulp work contributed by Aaron Weitekamp

---
+++ atomicpulp/__init__.py
@@ -0,0 +1,45 @@
+import sys, os
+from pulp import PulpServer
+from config import PulpConfig
+
+def writeOut(output, lf="\n"):
+    sys.stdout.flush()
+    sys.stdout.write(str(output) + lf)
+
+def push_image_to_pulp(image, server_url, username, password, verify_ssl, docker_client):
+    if not image:
+        raise ValueError("Image required")
+    parts = image.split("/")
+    if parts > 1:
+        if parts[0].find(".") != -1:
+            server_url = parts[0]
+            image = ("/").join(parts[1:])
+
+    repo = image.replace("/","-")
+    if not server_url:
+        raise ValueError("Server url required")
+
+    if not server_url.startswith("http"):
+        server_url = "https://" + server_url
+
+    try:
+        pulp = PulpServer(server_url=server_url, username=username,
+                          password=password, verify_ssl=verify_ssl, docker_client=docker_client)
+    except Exception as e:
+        raise IOError('Failed to initialize Pulp: {0}'.format(e))
+
+    try:
+        if not pulp.is_repo(repo):
+            pulp.create_repo(image, repo)
+    except Exception as e:
+        raise IOError('Failed to create Pulp repository: {0}'.format(e))
+        
+    try:
+        writeOut('Uploading image "{0}" to pulp server "{1}"'.format(image, server_url))
+        pulp.upload_docker_image(image, repo)
+        writeOut("")
+    except Exception as e:
+        raise IOError('Failed to upload image to Pulp: {0}'.format(e))
+
+    pulp.publish_repo(repo)
+    pulp.export_repo(repo)
