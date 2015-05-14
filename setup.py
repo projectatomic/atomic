536349ae0441d17a080a4f2edb436f59a50e5dde
Dan Walsh <dwalsh@redhat.com>
2015-05-14 10:45:35 -0400

    Move the Atomic class into python library directory.
    I am beginning to work on dbus bindings, this will allow the atomic cli and
    the atomic_dbus_server app to share the same code.
    
    Renamed atomicpulp to Atomic in the python library.

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@
 import os
 from distutils.core import setup
 
-setup(name = "atomic", scripts=["atomic"], version="1.0", description="Atomic Management Tool", author="Daniel Walsh", author_email="dwalsh@redhat.com", packages=["atomicpulp"])
+setup(name = "atomic", scripts=["atomic"], version="1.0", description="Atomic Management Tool", author="Daniel Walsh", author_email="dwalsh@redhat.com", packages=["Atomic"])
