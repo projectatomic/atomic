9092885f97a7e49476e1ad363801a3654c3f90df
Ratnadeep Debnath <rtnpro@gmail.com>
2015-08-11 00:10:36 +0530

    Add install_requires in setup.py from requirements.txt
    to install Python dependencies during setup.py develop/install.

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ import pkg_resources
 
 __version__ = pkg_resources.require('Atomic')[0].version
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="atomic", scripts=["atomic", "atomic_dbus.py"], version=__version__,
     description="Atomic Management Tool",
     author="Daniel Walsh", author_email="dwalsh@redhat.com",
     packages=["Atomic"],
+    install_requires=requirements,
     data_files=[('/etc/dbus-1/system.d/', ["org.atomic.conf"]),
                 ('/usr/share/dbus-1/system-services', ["org.atomic.service"]),
                 ('/usr/share/polkit-1/actions/', ["org.atomic.policy"]),
