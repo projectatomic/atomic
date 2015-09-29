74eee87353682aeaff061246af71261b0d6fe195
Dan Walsh <dwalsh@redhat.com>
2015-09-29 15:04:41 -0400

    Remove satellite_new.py from install
    Bump version number.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="atomic", scripts=["atomic", "atomic_dbus.py"],
-    version='1.4',
+    version='1.5',
     description="Atomic Management Tool",
     author="Daniel Walsh", author_email="dwalsh@redhat.com",
     packages=["Atomic"],
