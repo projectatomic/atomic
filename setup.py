4b4dcb32c73508827e41904f82ac8430a6ba98aa
Brent Baude <bbaude@redhat.com>
2015-11-04 13:12:15 -0600

    Do not build atomic with an egg file
        Switch from setuptools to distutils for setup which by
        default will not build things into eggs.  Rather it will
        install into PYDIR/site-apackages/<package>.  This makes
        debugging easier particularily on the Atomic environments.

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 
 # Author: Dan Walsh <dwalsh@redhat.com>
-import sys
-import os
-from setuptools import setup
+from distutils.core import setup
+import Atomic as _Atomic
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name="atomic", scripts=["atomic", "atomic_dbus.py"],
-    version='1.6',
-    description="Atomic Management Tool",
-    author="Daniel Walsh", author_email="dwalsh@redhat.com",
+    version = _Atomic.__version__,
+    author = _Atomic.__author__,
+    author_email = _Atomic.__author_email__,
     packages=["Atomic"],
-    install_requires=requirements,
     data_files=[('/etc/dbus-1/system.d/', ["org.atomic.conf"]),
                 ('/usr/share/dbus-1/system-services', ["org.atomic.service"]),
                 ('/usr/share/polkit-1/actions/', ["org.atomic.policy"]),
