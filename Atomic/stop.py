59638f456269a19a49662abcf2968a76458e2b72
Dan Walsh <dwalsh@redhat.com>
2016-08-30 05:44:52 -0400

    Split out atomic stop functions into their own source file.
    Create a Stop class as a subclass of Atomic and move all of the
    functions and CLI to this file.
    
    Closes: #570
    Approved by: baude

---
+++ Atomic/stop.py
@@ -0,0 +1,49 @@
+import argparse
+
+from . import util
+from .util import DockerObjectNotFound
+
+try:
+    from . import Atomic
+except ImportError:
+    from atomic import Atomic # pylint: disable=relative-import
+
+def cli(subparser):
+    # atomic stop
+    stopp = subparser.add_parser(
+        "stop", help=_("execute container image stop method"),
+        epilog="atomic will just stop the container if it is running, if "
+        "image does not specify LABEL STOP")
+    stopp.set_defaults(_class=Stop, func='stop')
+    util.add_opt(stopp)
+    stopp.add_argument("-n", "--name", dest="name", default=None,
+                       help=_("name of container"))
+    stopp.add_argument("image", help=_("container image"))
+    stopp.add_argument("args", nargs=argparse.REMAINDER,
+                          help=_("Additional arguments appended to the image "
+                                 "stop method"))
+
+class Stop(Atomic):
+    def __init__(self):
+        super(Stop, self).__init__()
+
+    def stop(self):
+        self.inspect = self._inspect_container()
+        if self.inspect is None:
+            self.inspect = self._inspect_image()
+            if self.inspect is None:
+                raise DockerObjectNotFound(self.name)
+
+        args = self._get_args("STOP")
+        if args:
+            cmd = self.gen_cmd(args + self.quote(self.args.args))
+            cmd = self.sub_env_strings(cmd)
+            self.display(cmd)
+            util.check_call(cmd, env=self.cmd_env())
+
+        # Container exists
+        try:
+            if self.inspect["State"]["Running"]:
+                self.d.stop(self.name)
+        except KeyError:
+            pass
