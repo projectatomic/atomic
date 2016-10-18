1a1d32c33642f57ea7a7a9da1fe8d0b396289a84
Jonathan Lebon <jlebon@redhat.com>
2016-10-18 11:03:52 -0400

    rhci.sh: don't install requirements.txt
    It's actually unnecessary here since we're already installing all the
    packages needed just before.
    
    And anyway, we should be using the packaged equivalents to ensure
    compatibility with what we'll actually be running against.
    
    Closes: #713
    Approved by: cgwalters

--- rhci.sh
+++ rhci.sh
@@ -28,11 +28,11 @@ dnf install -y \
   python3-slip-dbus \
   python3-docker-py \
   python3-gobject-base \
+  python2-dateutil \
   python3-dateutil \
   python2-coverage \
   attr
 
-pip install -r requirements.txt
 make pylint-check
 make test-python3-pylint
 make PYTHON=python3 PYLINT=true install DESTDIR=/host
