eccc9739a6dab0c9fa6a3558f4ca7c5e7fe182b9
Dan Walsh <dwalsh@redhat.com>
2016-11-07 10:20:22 -0500

    Bump to V1.14.0-dev

--- Atomic/__init__.py
+++ Atomic/__init__.py
@@ -5,7 +5,7 @@ from .util import write_out
 
 #https://bitbucket.org/logilab/pylint/issues/36/
 #pylint: disable=no-member
-__version__ = '1.13.8'
+__version__ = '1.14.0-dev'
 __author__  = 'Daniel Walsh'
 __author_email__ = 'dwalsh@redhat.com>'
 
