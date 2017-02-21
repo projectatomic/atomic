23f4bc914315deb16a40046ff210841bbfffa000
Brent Baude <bbaude@redhat.com>
2017-02-21 13:20:14 -0600

    Fix uninstall bug: BZ 1425495
    The bug https://bugzilla.redhat.com/show_bug.cgi?id=1425495 demostrates
    a case where atomic attempts to "add" a list and str resulting in a
    TypeError exception.  This should resolve that BZ.
    
    Closes: #903
    Approved by: rhatdan

--- Atomic/backends/_docker.py
+++ Atomic/backends/_docker.py
@@ -375,7 +375,10 @@ class DockerBackend(Backend):
 
         cmd = []
         if uninstall_command:
-            cmd =+ uninstall_command
+            try:
+                cmd =+ uninstall_command
+            except TypeError:
+                cmd = cmd + uninstall_command.split()
         if command_line_args:
             cmd += command_line_args
 
