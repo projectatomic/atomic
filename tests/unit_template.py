640fccc1b85406f9a40a83f81c0137a9eb466774
Steve Milner <smilner@redhat.com>
2017-05-22 10:38:48 -0400

    test: Update unit test template
    This change adds documentation areas and the pylint skip used in the
    other tests.
    
    Closes: #1009
    Approved by: giuseppe

--- tests/unit_template.py
+++ tests/unit_template.py
@@ -1,12 +1,19 @@
 #
 # Project Atomic Unit Test Example Template
 #
-
+#pylint: skip-file
 import unittest
 
 
 class TestAtomicUnit(unittest.TestCase):
+    """
+    TODO: This test case ...
+    """
+
     def test_unit_desired_behavior(self):
+        """
+        TODO: Verifies ...
+        """
         self.assertTrue(True)
 
 
